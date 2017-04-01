d8f741b19ecce69e17b760fcad9650ec27a93497
squiddity <squiddity@users.noreply.github.com>
2017-04-01 16:04:51 -0700

    timezone fix

--- recorder/recorder.sh
+++ recorder/recorder.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+cp /usr/share/zoneinfo/$TZ /etc/localtime
+
 mkdir -p /data/$CAMERA_NAME
 
-exec ffmpeg -rtsp_transport tcp -i rtsp://$CAMERA_USER:$CAMERA_PW@$CAMERA_IP:554 -an -vcodec copy -map_metadata -1 -f segment -segment_time 60 -segment_format mp4 -strftime 1 -reset_timestamps 1 "/data/$CAMERA_NAME/%Y-%m-%d_%H-%M-%S.mp4"
+exec ffmpeg -rtsp_transport tcp -i rtsp://$CAMERA_USER:$CAMERA_PW@$CAMERA_IP:554 -an -vcodec copy -map_metadata -1 -f segment -segment_time 600 -segment_format mp4 -strftime 1 -reset_timestamps 1 "/data/$CAMERA_NAME/%Y-%m-%d-%H-%M-%S.mp4"
