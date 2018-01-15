fcacf584395125b9e71a4ffe552f33982571090c
squiddity <squiddity@users.noreply.github.com>
2018-01-15 13:11:07 -0800

    adding xerror stimeout which is in millis

--- recorder/recorder.sh
+++ recorder/recorder.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#/bin/sh
 
 cp /usr/share/zoneinfo/$TZ /etc/localtime
 
 mkdir -p /data/$CAMERA_NAME
 
-exec ffmpeg -rtsp_transport tcp -i rtsp://$CAMERA_USER:$CAMERA_PW@$CAMERA_IP:554 -an -vcodec copy -map_metadata -1 -f segment -segment_time 600 -segment_format mp4 -strftime 1 -reset_timestamps 1 "/data/$CAMERA_NAME/%Y-%m-%d-%H-%M-%S.mp4"
+exec ffmpeg -xerror -stimeout 2000000 -rtsp_transport tcp -i rtsp://$CAMERA_USER:$CAMERA_PW@$CAMERA_IP:554 -an -vcodec copy -map_metadata -1 -f segment -segment_time 600 -segment_format mp4 -strftime 1 -reset_timestamps 1 "/data/$CAMERA_NAME/%Y-%m-%d-%H-%M-%S.mp4"
