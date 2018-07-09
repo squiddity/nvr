0f9570fae5590086f7166aed18acde01c9b8a996
squiddity <squiddity@users.noreply.github.com>
2018-07-08 19:10:08 -0700

    fixing initial timestamp errors by using recorder clock

--- recorder/recorder.sh
+++ recorder/recorder.sh
@@ -4,4 +4,4 @@ cp /usr/share/zoneinfo/$TZ /etc/localtime
 
 mkdir -p /data/$CAMERA_NAME
 
-exec ffmpeg -xerror -stimeout 2000000 -rtsp_transport tcp -i rtsp://$CAMERA_USER:$CAMERA_PW@$CAMERA_IP:554 -an -vcodec copy -map_metadata -1 -f segment -segment_time 600 -segment_format mp4 -strftime 1 -reset_timestamps 1 "/data/$CAMERA_NAME/%Y-%m-%d-%H-%M-%S.mp4"
+exec ffmpeg -xerror -stimeout 2000000 -use_wallclock_as_timestamps 1 -rtsp_transport tcp -i rtsp://$CAMERA_USER:$CAMERA_PW@$CAMERA_IP:554 -an -vcodec copy -map_metadata -1 -f segment -segment_time 600 -segment_format mp4 -strftime 1 -reset_timestamps 1 "/data/$CAMERA_NAME/%Y-%m-%d-%H-%M-%S.mp4"
