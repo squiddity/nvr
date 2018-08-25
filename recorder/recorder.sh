bb028eafadd9b6f27122449d47ac0e3f6628322a
squiddity <squiddity@users.noreply.github.com>
2018-08-24 22:15:30 -0700

    fixing shell script

--- recorder/recorder.sh
+++ recorder/recorder.sh
@@ -1,7 +1,11 @@
-#/bin/sh
+#!/bin/sh
 
 cp /usr/share/zoneinfo/$TZ /etc/localtime
 
 mkdir -p /data/$CAMERA_NAME
+
+while ! nc -z $CAMERA_IP 554; do   
+  sleep 0.5
+done
 
 exec ffmpeg -xerror -stimeout 2000000 -use_wallclock_as_timestamps 1 -rtsp_transport tcp -i rtsp://$CAMERA_USER:$CAMERA_PW@$CAMERA_IP:554 -an -vcodec copy -map_metadata -1 -f segment -segment_time 600 -segment_format mp4 -strftime 1 -reset_timestamps 1 "/data/$CAMERA_NAME/%Y-%m-%d-%H-%M-%S.mp4"
